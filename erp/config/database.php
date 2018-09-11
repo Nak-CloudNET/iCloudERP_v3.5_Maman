3cddff9e02d47f1dce3dcc29cbb270ca15691c9c
Nak <khat.ratanak@cloudnet.com.kh>
2018-09-11 12:01:41 +0700

    Nak upload system maman

--- erp/config/database.php
+++ erp/config/database.php
@@ -61,28 +61,28 @@
     $query_builder = TRUE;
     
     $db['default'] = array(
-                           'dsn'    => '',
-                           'hostname' => 'localhost',
-                           'username' => 'cloudmo2_maman',
-                           'password' => 'CloudNET#168',
-                           'database' => 'cloudmo2_maman_data',
-                           'dbdriver' => 'mysqli',
-                           'dbprefix' => 'erp_', 
-                           'pconnect' => FALSE,
-                           'db_debug' => TRUE,
-                           'cache_on' => FALSE,
-                           'cachedir' => '',
-                           //'char_set' => 'utf8mb4',
-                           //'dbcollat' => 'utf8mb4_unicode_ci',
-                           'char_set' => 'utf8',
-                           'dbcollat' => 'utf8_general_ci',
-                           'swap_pre' => '',
-                           'encrypt' => FALSE,
-                           'compress' => FALSE,
-                           'stricton' => FALSE,
-                           'failover' => array(),
-                           'save_queries' => FALSE
-                           );
+       'dsn'    => '',
+       'hostname' => 'localhost',
+       'username' => 'root',
+       'password' => '',
+       'database' => 'iclouderp_v3_5_maman',
+       'dbdriver' => 'mysqli',
+       'dbprefix' => 'erp_',
+       'pconnect' => FALSE,
+       'db_debug' => TRUE,
+       'cache_on' => FALSE,
+       'cachedir' => '',
+       //'char_set' => 'utf8mb4',
+       //'dbcollat' => 'utf8mb4_unicode_ci',
+       'char_set' => 'utf8',
+       'dbcollat' => 'utf8_general_ci',
+       'swap_pre' => '',
+       'encrypt' => FALSE,
+       'compress' => FALSE,
+       'stricton' => FALSE,
+       'failover' => array(),
+       'save_queries' => FALSE
+       );
     
     
     /* End of file database.php */
